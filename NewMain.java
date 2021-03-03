c520bb9c074bdbd73ffb84f3bec36d6b3be22581
Jordan K <kuschnerj@gmail.com>
2021-03-03 13:47:32 -0800

    Update NewMain.java

--- NewMain.java
+++ NewMain.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author jskuschner
- */
-
 import java.util.Random; 
 
 public class NewMain {
@@ -21,36 +10,27 @@ public class NewMain {
     public static void main(String[] args) {
         
         BinaryTree test = new BinaryTree();
-        final int riaSIZE = 15;
+        final int riaSIZE = 200;
         final int randNumOfNodesToDelete = 5;
         int[] randomIntegerArray = new int[riaSIZE];
         Random rand = new Random();
         
+        
         for(int i = 0; i < riaSIZE; i++){
             randomIntegerArray[i] = rand.nextInt(100);
+        
             test.add(randomIntegerArray[i]);
         }
-        
-        test.printBinaryTree(test.root, 0);
-        
-        for(int i = 0; i <= randNumOfNodesToDelete; i++){
-            int x = rand.nextInt(riaSIZE - 1);
-            System.out.println("Deleting: " + randomIntegerArray[x]);
-            test.delete(randomIntegerArray[x]);
-        }
-        System.out.println("**********************************************");
-        System.out.println("**********************************************");
-      
-        test.printBinaryTree(test.root, 0);
-        System.out.println("Adding Repeated Node: " + test.root.data);
-        test.add(test.root.data);
-        test.printBinaryTree(test.root, 0);
+        System.out.println();
+                test.printBinaryTree(test.root, 0);
 
-        test.contains(13);
-        System.out.println("Deleting: " + test.root.data);
-        test.delete(test.root.data);
+         for(int i = 0; i < riaSIZE; i++){
+            test.delete(randomIntegerArray[i]);
+        }
+         if(test.root == null){
+             System.out.print("The tree is empty.");
+         }
         test.printBinaryTree(test.root, 0);
-        
         
     }
     
